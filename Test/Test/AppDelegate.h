f528e20bf1af01020e1734b4c5e5ab4b0db43427
Alex Gurin <evil_goga@bk.ru>
2013-10-18 08:47:03 +0300

    test

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Алексей on 18.10.13.
+//  Copyright (c) 2013 PrivatBank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Test/Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Алексей on 18.10.13.
+//  Copyright (c) 2013 PrivatBank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
